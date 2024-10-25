f484515f4cd3baf52334861876a238a0ef1d2c8f
jayseeeeee <dacasin.johnchristian@outlook.com>
2024-10-25 19:46:56 +0800

    style: rename folders

--- Fundamentals of Programming/Week 11 /NamesArrayList.java
+++ Fundamentals of Programming/Week 11 /NamesArrayList.java
@@ -7,7 +7,7 @@ public class NamesArrayList {
         while (names.size() != 6) {
             System.out.print("Please enter a name: ");
             names.add(input.nextLine());
-        }
+        }  
         System.out.println(names);
         int size = names.size();
         for (int i = 1; i < size*2; i += 2) {
